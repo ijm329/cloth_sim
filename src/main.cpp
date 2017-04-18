6fd569282a1420adeadbc35d108608b9e10f2239
Karthic Palaniappan <kapalani@andrew.cmu.edu>
2017-04-17 23:56:52 -0400

    Adding keyboard functions

--- src/main.cpp
+++ src/main.cpp
@@ -13,16 +13,47 @@
 
 void renderScene()
 {
+    static float angle = 0.0f;
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor4f(0.2f, 0.2f, 0.4f, 0.5f);
+    glLoadIdentity();
+    gluLookAt(0.0f, 0.0f, 10.0f,
+              0.0f, 0.0f, 0.0f,
+              0.0f, 1.0f, 0.0f);
+    glRotatef(angle, 0.0f, 1.0f, 0.0f);
+
+    glColor3f(0.2f, 0.2f, 0.4f);
 
     glBegin(GL_TRIANGLES);
-    glVertex3f(0.0, 0.5, 0.0);
-    glVertex3f(-0.5, -0.5, 0.0);
-    glVertex3f(0.5, -0.5, 0.0);
+    glVertex3f(-2.0, -2.0, -5.0);
+    glVertex3f(2.0, 0.0, -5.0);
+    glVertex3f(0.0, 2.0, -5.0);
     glEnd();
 
+    angle += 0.1f;
+
     glutSwapBuffers();
+}
+
+void resizeWindow(int width, int height)
+{
+    height = (height == 0) ? 1 : height;
+    float ratio = 1.0 * width / height;
+    glMatrixMode(GL_PROJECTION);
+
+    glLoadIdentity();
+    glViewport(0, 0, width, height);
+
+    gluPerspective(45, ratio, 1, 1000);
+
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void processKeys(unsigned char key, int x, int y)
+{
+    //x and y are mouse position relative to top left corner when key is pressed
+    if((key == 'q') || (key == 'Q') || (key == 27))
+        exit(0);
 }
 
 void printVersionInfo()
@@ -32,9 +63,15 @@ void printVersionInfo()
                " OpenGL version: " <<glGetString(GL_VERSION) << std::endl;
 }
 
+void glInit()
+{
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+}
+
 int main(int argc, char **argv)
 {
-
     //initialize GLUT and create window
     glutInit(&argc, argv);
     glutInitWindowSize(DEFAULT_W, DEFAULT_H);
@@ -49,8 +86,13 @@ int main(int argc, char **argv)
     }
     printVersionInfo();
 
+    glInit();
+
     //register GLUT callbacks
     glutDisplayFunc(renderScene);
+    glutReshapeFunc(resizeWindow);
+    glutIdleFunc(renderScene);
+    glutKeyboardFunc(processKeys);
 
     //enter GLUT event processing cycle
     glutMainLoop();
